276d7d2e93d2568e30c67e6a4a6d5e16876cf026
Garrett Estrin <garrett.estrin@gmail.com>
2016-12-11 22:24:06 -0800

    added all users route

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const
 
   jwt = require('jsonwebtoken'),
   User = require('./models/user.js'),
+  apiRoutes = express.Router(),
   PORT = process.env.PORT || 3000
 
 mongoose.Promise = global.Promise
@@ -48,6 +49,27 @@ app.get('/setup', function(req, res) {
     res.json({ success: true });
   });
 });
+
+// API ROUTES -------------------
+
+// TODO: route to authenticate a user (POST http://localhost:8080/api/authenticate)
+
+// TODO: route middleware to verify a token
+
+// route to show a random message (GET http://localhost:8080/api/)
+apiRoutes.get('/', function(req, res) {
+  res.json({ message: 'Welcome to the coolest API on earth!' });
+});
+
+// route to return all users (GET http://localhost:8080/api/users)
+apiRoutes.get('/users', function(req, res) {
+  User.find({}, function(err, users) {
+    res.json(users);
+  });
+});
+
+// apply the routes to our application with the prefix /api
+app.use('/api', apiRoutes);
 
 app.listen(PORT, function(){
   console.log("Connected on port " + PORT);
